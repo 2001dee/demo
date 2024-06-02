a467e14b40083516138bb585b2ded4a6ee62e206
2001dee <139195049+2001dee@users.noreply.github.com>
2024-06-02 08:26:49 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,41 @@
+console.log("Welcome to Spotify");
+
+// Initialize the Variables
+let songIndex = 0;
+let audioElement = new Audio('1.mp3');
+let masterPlay = document.getElementById('masterPlay');
+let myProgressBar = document.getElementById('myProgressBar');
+let gif = document.getElementById('gif');
+
+let song =[
+    {songName:"salam-e-Ishq",filePath:"song/1.mp3", coverPath:"covers/cover2.jpg"}
+    
+]
+//audioElement.play();
+
+//Handle play/puse click
+masterPlay.addEventListener('click',()=>{
+    if (audioElement.paused || audioElement.currentTime<=0){
+        audioElement.play();
+        masterPlay.classList.remove('fa-circle-play');
+        masterPlay.classList.add('fa-circle-pause');
+        gif.style.opacity = 1;
+    }
+    else{
+        audioElement.pause();
+        masterPlay.classList.remove('fa-circle-pause');
+        masterPlay.classList.add('fa-circle-play');
+        gif.style.opacity = 0;
+    }
+})
+
+// Listen to Events
+audioElement.addEventListener('timeupdate' ,()=>{
+    //updatee seekbaar
+    progress = parseInt((audioElement.currentTime/audioElement.duration)*100);
+    myProgressBar.value = progress;
+})
+
+myProgressBar.addEventListener('change',()=>{
+    audioElement.currentTime = myProgressBar.value * audioElement.duration;
+})
